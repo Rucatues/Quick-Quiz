fc49a75172a6bafb4eb5a1c81cfae052135e26ef
Rucatues <115330764+Rucatues@users.noreply.github.com>
2023-01-06 21:52:39 -0600

    initials and score stored in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,16 @@ const endContainer = document.querySelector('.end-container'); //container that 
 const submitBtn = document.querySelector(".submitBtn"); //button that submits your initials to local storage
 const initialsBox = document.getElementById('initials-box') //box where you put your initials 
 const finalScore = document.getElementById('final-score') //targets the final score
+const highScore = document.querySelector('.high-score')
 let info = []
-let globalIndex = 0;
 let score = 0;
 
+const storageArray = JSON.parse(localStorage.getItem('quiz game'))
+if (storageArray !== null) {
+    info = storageArray;
+}
+console.log(info)
+console.log(storageArray);
 const questions = [
     {
         questionText: "What does CSS stand for?",
@@ -40,46 +46,51 @@ function startGame() {
     console.log('Game started.');
     startButton.classList.add('hide');
     answerButtons.classList.remove('hide');
-    giveFirstQuestion(globalIndex);
+    giveFirstQuestion(0);
 }
 
 function giveFirstQuestion(globalIndex) {
-    text.innerText = questions[globalIndex].questionText;
-    answer1.innerHTML = questions[globalIndex].choices[0];
-    answer2.innerHTML = questions[globalIndex].choices[1];
-    answer3.innerHTML = questions[globalIndex].choices[2]
+    text.innerText = questions[globalIndex]?.questionText;
+    answer1.innerHTML = questions[globalIndex]?.choices[0];
+    answer2.innerHTML = questions[globalIndex]?.choices[1];
+    answer3.innerHTML = questions[globalIndex]?.choices[2]
     answer1.addEventListener('click', (event) => {
-        event.preventDefault();
-        selectAnswer(answer1.innerText);
+
+        selectAnswer(answer1.innerText, globalIndex);
     });
     answer2.addEventListener('click', (event) => {
-        event.preventDefault();
-        selectAnswer(answer2.innerText);
+
+        selectAnswer(answer2.innerText, globalIndex);
     });
     answer3.addEventListener('click', (event) => {
-        event.preventDefault();
-        selectAnswer(answer3.innerText);
+
+        selectAnswer(answer3.innerText, globalIndex);
     });
 
 }
 
-function selectAnswer(selectedAnswer) {
+function selectAnswer(selectedAnswer, index) {
     console.log(selectedAnswer);
-    if (selectedAnswer === questions[globalIndex]?.correctAnswer) {
+    if (selectedAnswer === questions[index]?.correctAnswer) {
         console.log('Correct')
         score += 5;
     } else {
         console.log('Wrong')
     }
-    globalIndex++;
-    console.log(globalIndex);
-    if (globalIndex < questions.length) {
+
+    console.log(index);
+    index++;
+    goToNextQuestion(index);
+}
+function goToNextQuestion(index) {
+    console.log(index);
+    if (index <= questions.length) {
         nextButton.classList.remove('hide');
         nextButton.addEventListener('click', (event) => {
             event.preventDefault();
-            if (globalIndex < questions.length) {
-                giveFirstQuestion(globalIndex);
-            } else {
+            if (index < questions.length) {
+                giveFirstQuestion(index);
+            } else if (index === questions.length) {
                 console.log('Game Over');
                 gameOver();
             }
@@ -91,9 +102,32 @@ function gameOver() {
     questionContainer.classList.add('hide');
     nextButton.classList.add('hide');
     endContainer.classList.remove('hide');
-    console.log(score)
+    console.log(score);
+    storeData();
 }
 
+function storeData() {
+    submitBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        // console.log(initialsBox.value);
+        const userObj = {
+            score: score,
+            initials: initialsBox.value
+        }
+        info.push(userObj);
+        localStorage.setItem('quiz game', JSON.stringify(info))
+        displayHighScore();
+    })
+}
 
-
-// Submit initials and score to local storage//
+function displayHighScore() {
+    for (let i = 0; i < info.length; i++) {
+        console.log(info[i]);
+        var listItem = document.createElement('li');
+        // let arrayOfInfo = Object.entries(info[i]) //trying to list the object entries in an array
+        // let sortedScores = arrayOfInfo.sort((a, b) => b[1] - a[1])   //to then sort them
+        // console.log(sortedScores)
+        listItem.textContent = info[i].initials + ' ' + info[i].score; /* and then would need to change info to sortedScores here?*/
+        highScore.appendChild(listItem);
+    }
+}
